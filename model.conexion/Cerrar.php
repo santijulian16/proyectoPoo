cd1c24e05735ed491f5577e3bbf0bd46502eb4e8
Yesica <Yesica@DESKTOP-2942UU4>
2018-04-29 18:49:59 -0500

    Temporazador de sesion

---
+++ model.conexion/Cerrar.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    session_destroy(); 
+    session_unset();
+
+    echo "<script>
+            alert('Gracias por visitarnos');
+            location.href='index.php';
+         </script>";
+    exit;
+
+?>
