fe1db41aae2a058de60979e8ae1000c2f6908814
Yesica <Yesica@DESKTOP-2942UU4>
2018-04-30 00:00:50 -0500

    Modificacion de la pagina inicial

--- PaginaInicio.php
+++ PaginaInicio.php
@@ -14,40 +14,9 @@ include './model.DAO/usuariosDAO.php';
     <head><title>Pagina Inicio</title></head>
         <body style="background-color: #E0E0E0;">
 
-            <?php
-            /*
-               $sql = "SELECT a.nombre nombre,a.url url FROM usuarios u, usu_apli up where a.id=up.id_apli and up.id_usuario=u.id and u.id=$id";
-                $result = $conn->query($sql);
-                $total=$result->num_rows;
-                if($total>0)
-                {
-                        echo "
-                        <table border=0>
-                        <tr>
-                        <td>mis botones</td>
-                        </tr>
-                        ";
-
-                        for($i=1;$i<=$total;$i++)
-                        {
-                                $row = $result->fetch_assoc();
-                                $nombre_c=$row["nombre"];
-                                $url_c=$row["url"];
-
-                                echo "
-                                <tr>
-                                        <td align=center><input type=button value='$nombre_c' onclick=cargar('$url_c');></td>
-                                </tr>
-                                ";
-                        }
-                        echo "
-                        </table>
-                        ";
-                }	*/
-            ?>
-                    <div class="col-md-6 pull-left">
-                        <a><input type="button" value="Salir" onclick="Salir();"></a>
-                    </div>
+            <div class="col-md-6 pull-left" style="padding: 40px;">
+                <a align="right" href="index.php" class="btn btn-success"><i class="fa fa-sign-in"></i> Cerrar Sesion </a>
+            </div>
 
             <div class="container">
                 <div class="row">
@@ -55,35 +24,62 @@ include './model.DAO/usuariosDAO.php';
                         <table width="100%" heigth="100%" border="0">
                             <tbody>
                                 <tr heigth="10%">
-                                    <td width="20%" align="center">MIS BOTONES</td>
-                                    <td><div>BIENVENIDO: <?php //echo $_SESSION["nombre"];?></div></td>
+                                    <td width="20%" align="center" style="background-color: #FAD7A0">MIS BOTONES</td>
+                                    <td><div style="padding: 15px; background-color: #82E0AA">BIENVENIDO <?php //echo $_SESSION["nombre"];?></div></td>
                                 </tr>
                                 <tr height="90%">
                                     <td valign="top" align="center">
+                                        <?php
+                                        /*
+                                            $sql = "SELECT a.nombre nombre,a.url url FROM usuarios u,aplicaciones a, usu_apli up where a.id=up.id_apli and up.id_usuario=u.id and u.id=$id";
+                                            $result = $con;
+                                            $total=$result->num_rows;
+                                            if($total>0)
+                                            {
+                                                    echo "
+                                                    <table border=0>";
+
+                                                    for($i=1;$i<=$total;$i++)
+                                                    {
+                                                            $row = $result->fetch_assoc();
+                                                            $nombre_c=$row["nombre"];
+                                                            $url_c=$row["url"];
+
+                                                            echo "
+                                                            <tr>
+                                                                    <td align=center><input type=button value='$nombre_c' onclick=cargar('$url_c');></td>
+                                                            </tr>
+                                                            ";
+                                                    }
+                                                    echo "
+                                                    </table>
+                                                    ";
+                                            }	*/
+                                        ?>
                                         <table border="0">
                                             <tbody>
                                                 <tr>
-                                                    <td align="center">
+                                                    <td align="right">
                                                         <input type="button" value="Asignacion de permisos" onclick="cargar('permisos.php')">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>
+                                                    <td align="right">
                                                         <input type="button" value="Docentes" onclick="cargar('docentes.php')">   
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td> 
+                                                    <td align="right"> 
                                                         <input type="button" value="Programas" onclick="cargar('programas.php')"> 
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>
+                                                    <td align="right">
                                                         <input type="button" value="Salones" onclick="cargar('salones.php')"> 
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>
+                                                    <td align="right">
                                                         <input type="button" value="Edificios" onclick="cargar('Edificios.php')"> 
                                                     </td>
                                                 </tr> 
