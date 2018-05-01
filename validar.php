d38ff55a9b4de17bc2dfd47fa1b1f82e9f105935
NE$$ 97 <NE$$ 97@DESKTOP-I8JN04B>
2018-04-30 21:51:38 -0500

    Iniciar formulario de login

---
+++ validar.php
@@ -0,0 +1,41 @@
+<?php
+  session_start();
+  
+  ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <head>
+        <title>VALIDANDO...</title>
+        <meta chaset="utf-8"
+    </head>        
+    </head>   
+    <body>
+        <?php 
+            include './model.conexion/Conexion.php';
+            if(isset($_POST['login'])){
+                $usuario = $_POST['user'];
+                $pw = $_POST['pw'];
+                $log = mysql_query("SELECT * FROM usuarios WHERE documento='$usuario' AND clave='$pw'");
+                if (mysql_num_rows($log)>0){
+                    $row = mysql_fetch_array($log);
+                    $_SESSION["user"] = $row['user'];
+                   echo '<script window.location="panel.php"; </script>';
+                }
+                else{
+                    echo '<script> alert("Usuario o contraseña son incorrectos.");</script>';
+                    echo '<script> window.location="iniciarSesion.php"; </script>';
+                    
+                
+                 
+               }
+            }
+            
+         ?>
+        
+    </body>
+</html>
+
+
+
